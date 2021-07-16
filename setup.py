ecdcc1f2840c7eecefdc285d35a94ad5ade936ef
Luc Perard (LUEA) <luea@oticonmedical.com>
2021-07-16 16:07:38 +0200

    master ON/OFF added to power supply

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ base = None
 if sys.platform == 'win32':
     base = 'Win32GUI'
 
-includefiles = ['parameters.json', 'Metadata.json', 'Error.json', 'Devices.json', 'NIVISA_19.5.exe', 'ReadMe.txt', 'HMC8042.png']
+includefiles = ['parameters.json', 'Metadata.json', 'Error.json', 'Devices.json', 'NIVISA_19.5.exe', 'ReadMe.txt', 'HMC8042.png', '2220-30-1.png']
 
 executables = [
     Executable('MyLab.py', base=base, icon='icon2.ico')
