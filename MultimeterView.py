6a878f0cf2c4e4c59029fc34c1e44cdc26bded98
unknown <lpl.perard@gmail.com>
2021-07-26 22:50:22 +0200

    GUI improvement Multimeter

--- MultimeterView.py
+++ MultimeterView.py
@@ -50,7 +50,7 @@ class MultimeterView (DeviceFrame):
     #This methods initiates all attributes in the class. It is usefull to prevent double usage     
         self.state="freeze"   
         self.labelFrame_setup = LabelFrame(self.frame, text="Setup")
-        self.canva_setup = Canvas(self.labelFrame_setup, scrollregion=(0,0,0,500), bd=0, highlightthickness=0, bg=self.model.parameters_dict['backgroundColor'])
+        self.canva_setup = Canvas(self.labelFrame_setup, scrollregion=(0,0,0,470), bd=0, highlightthickness=0, bg=self.model.parameters_dict['backgroundColor'])
         self.defilY_setup = Scrollbar(self.labelFrame_setup, orient='vertical', command=self.canva_setup.yview, bg=self.model.parameters_dict['backgroundColor'])
 
         self.frame_instrument_name = Frame(self.labelFrame_instrument)
@@ -70,6 +70,7 @@ class MultimeterView (DeviceFrame):
         self.frame_2WR = Frame(self.scrollframe_setup)
         self.frame_4WR = Frame(self.scrollframe_setup)
         self.frame_diode = Frame(self.scrollframe_setup)
+        self.frame_diode_caliber = Frame(self.scrollframe_setup) 
         self.frame_continuity = Frame(self.scrollframe_setup)
         self.frame_frequency = Frame(self.scrollframe_setup)
         self.frame_period = Frame(self.scrollframe_setup)
@@ -88,7 +89,9 @@ class MultimeterView (DeviceFrame):
         self.doubleVar_frequency = DoubleVar()
         self.doubleVar_period = DoubleVar()
         self.intVar_radioValueSetup = IntVar()
-        self.intVar_radioValueCaliber = IntVar()
+        self.intVar_radioValueCaliberA = IntVar()
+        self.intVar_radioValueCaliberDiodeV = IntVar()
+        self.intVar_radioValueCaliberDiodeA = IntVar()
         self.intVar_radioValuemeasure = IntVar()
 
         self.label_instrumentName = Label(self.frame_instrument_name, text="Name :")
@@ -96,9 +99,12 @@ class MultimeterView (DeviceFrame):
         
         self.label_DCI_caliber = Label(self.frame_DCI_caliber, text="Caliber :")
         self.label_ACI_caliber = Label(self.frame_ACI_caliber, text="Caliber :")
+        self.label_frequency_unit = Label(self.frame_frequency, text=" Hz")
+        self.label_period_unit = Label(self.frame_period, text=" s   ")
+        self.label_diode_unit = Label(self.frame_diode, text=" s   ")
 
-        self.entry_instrumentName = Entry(self.frame_instrument_name, textvariable=self.stringvar_instrumentName)
-        self.entry_instrumentaddress = Entry(self.frame_instrument_address, textvariable=self.stringvar_instrumentaddress, state="readonly")
+        self.entry_instrumentName = Entry(self.frame_instrument_name, textvariable=self.stringvar_instrumentName, width=25)
+        self.entry_instrumentaddress = Entry(self.frame_instrument_address, textvariable=self.stringvar_instrumentaddress, width=25, state="readonly")
 
         self.entry_DCV = Entry(self.frame_DCV, textvariable=self.doubleVar_DCV, state="readonly", width=10)
         self.entry_ACV = Entry(self.frame_ACV, textvariable=self.doubleVar_ACV, state="readonly", width=10)
@@ -106,9 +112,9 @@ class MultimeterView (DeviceFrame):
         self.entry_ACI = Entry(self.frame_ACI, textvariable=self.doubleVar_DCI, state="readonly", width=10)
         self.entry_2WR = Entry(self.frame_2WR, textvariable=self.doubleVar_2WR, state="readonly", width=10)
         self.entry_4WR = Entry(self.frame_4WR, textvariable=self.doubleVar_4WR, state="readonly", width=10)
-        self.entry_diode = Entry(self.frame_diode, textvariable=self.doubleVar_diode, state="readonly", width=20)
-        self.entry_frequency = Entry(self.frame_frequency, textvariable=self.doubleVar_frequency, state="readonly", width=20)
-        self.entry_period = Entry(self.frame_period, textvariable=self.doubleVar_period, state="readonly", width=20)
+        self.entry_diode = Entry(self.frame_diode, textvariable=self.doubleVar_diode, state="readonly", width=14)
+        self.entry_frequency = Entry(self.frame_frequency, textvariable=self.doubleVar_frequency, state="readonly", width=14)
+        self.entry_period = Entry(self.frame_period, textvariable=self.doubleVar_period, state="readonly", width=14)
 
         self.combo_DCV = Combobox(self.frame_DCV, state="readonly", width=8, values=["1V", "100mV", "1000V", "100V", "10V"])
         self.combo_ACV = Combobox(self.frame_ACV, state="readonly", width=8, values=["1V", "100mV", "750V", "100V", "10V"])
@@ -122,7 +128,7 @@ class MultimeterView (DeviceFrame):
         self.diodeImg = ImageTk.PhotoImage(self.diodeImg)
 
         self.continuityImg = Image.open("continuity.png")
-        self.continuityImg = self.continuityImg.resize((25, 22), Image.ANTIALIAS)
+        self.continuityImg = self.continuityImg.resize((25, 15), Image.ANTIALIAS)
         self.continuityImg = ImageTk.PhotoImage(self.continuityImg)        
 
         self.img = None
@@ -137,12 +143,17 @@ class MultimeterView (DeviceFrame):
         self.radio_diode = Radiobutton(self.frame_diode, image=self.diodeImg, variable=self.intVar_radioValueSetup, value=6, command=self.radio_setupState_callback)
         self.radio_continuity = Radiobutton(self.frame_continuity, image=self.continuityImg, variable=self.intVar_radioValueSetup, value=7, command=self.radio_setupState_callback)
         self.radio_frequency = Radiobutton(self.frame_frequency, text='Frequency', variable=self.intVar_radioValueSetup, value=8, command=self.radio_setupState_callback)
-        self.radio_period = Radiobutton(self.frame_period, text='Period', variable=self.intVar_radioValueSetup, value=9, command=self.radio_setupState_callback)
+        self.radio_period = Radiobutton(self.frame_period, text='Period     ', variable=self.intVar_radioValueSetup, value=9, command=self.radio_setupState_callback)
 
-        self.radio_caliberACmA = Radiobutton(self.frame_ACI_caliber, text='mA', variable=self.intVar_radioValueCaliber, value=0, command=self.radio_caliber_callback)
-        self.radio_caliberDCmA = Radiobutton(self.frame_DCI_caliber, text='mA', variable=self.intVar_radioValueCaliber, value=0, command=self.radio_caliber_callback)
-        self.radio_caliberAC10A = Radiobutton(self.frame_ACI_caliber, text='10A', variable=self.intVar_radioValueCaliber, value=1, command=self.radio_caliber_callback)
-        self.radio_caliberDC10A = Radiobutton(self.frame_DCI_caliber, text='10A', variable=self.intVar_radioValueCaliber, value=1, command=self.radio_caliber_callback)
+        self.radio_caliberACmA = Radiobutton(self.frame_ACI_caliber, text='mA', variable=self.intVar_radioValueCaliberA, value=0, command=self.radio_caliber_callback)
+        self.radio_caliberDCmA = Radiobutton(self.frame_DCI_caliber, text='mA', variable=self.intVar_radioValueCaliberA, value=0, command=self.radio_caliber_callback)
+        self.radio_caliberAC10A = Radiobutton(self.frame_ACI_caliber, text='10A', variable=self.intVar_radioValueCaliberA, value=1, command=self.radio_caliber_callback)
+        self.radio_caliberDC10A = Radiobutton(self.frame_DCI_caliber, text='10A', variable=self.intVar_radioValueCaliberA, value=1, command=self.radio_caliber_callback)
+
+        self.radio_caliberDiode5V = Radiobutton(self.frame_diode_caliber, text='5V', variable=self.intVar_radioValueCaliberDiodeV, value=0)
+        self.radio_caliberDiode10V= Radiobutton(self.frame_diode_caliber, text='10V', variable=self.intVar_radioValueCaliberDiodeV, value=1)
+        self.radio_caliberDiode5mA = Radiobutton(self.frame_diode_caliber, text='5mA', variable=self.intVar_radioValueCaliberDiodeA, value=0)
+        self.radio_caliberDiode10mA = Radiobutton(self.frame_diode_caliber, text='10mA', variable=self.intVar_radioValueCaliberDiodeA, value=1)
 
         self.measure_activate = Button(self.frame_measure_button, text='Measure ON/OFF', command=self.measure_activate_callback)
         self.radio_measureStateOFF = Radiobutton(self.frame_measure_radio, text='OFF', variable=self.intVar_radioValuemeasure, value=0)
@@ -162,7 +173,7 @@ class MultimeterView (DeviceFrame):
 
                 if self.model.devices_dict[item][0] == "8845A":   
                     self.img = Image.open(self.model.devices_dict[item][2])
-                    self.img = self.img.resize((225, 115), Image.ANTIALIAS)
+                    self.img = self.img.resize((200, 100), Image.ANTIALIAS)
                     self.img = ImageTk.PhotoImage(self.img)
                     self.panel = Label(self.frame, image = self.img, bg=self.model.parameters_dict['backgroundColor'])
                     self.panel.pack(fill = "both", expand = "yes")
@@ -204,7 +215,7 @@ class MultimeterView (DeviceFrame):
         self.canva_setup.create_window(0, 0, anchor='nw', window=self.scrollframe_setup)
         self.scrollframe_setup.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.canva_setup.config(yscrollcommand= self.defilY_setup.set, height=280, width=220)
+        self.canva_setup.config(yscrollcommand= self.defilY_setup.set, height=260, width=220)
         self.canva_setup.pack(side="left", fill="both")
         self.defilY_setup.pack(fill="y", side='left', padx='5')   
 
@@ -243,6 +254,9 @@ class MultimeterView (DeviceFrame):
         
         self.frame_diode.configure(bg=self.model.parameters_dict['backgroundColor'])
         self.frame_diode.pack(fill="both", pady=5)
+        
+        self.frame_diode_caliber.configure(bg=self.model.parameters_dict['backgroundColor'])
+        self.frame_diode_caliber.pack(fill="both", pady=5)
         
         self.frame_continuity.configure(bg=self.model.parameters_dict['backgroundColor'])
         self.frame_continuity.pack(fill="both", pady=5)
@@ -296,6 +310,15 @@ class MultimeterView (DeviceFrame):
 
         self.label_ACI_caliber.configure(bg=self.model.parameters_dict['backgroundColor'])
         self.label_ACI_caliber.pack(side="right", anchor='ne')
+
+        self.label_diode_unit.configure(bg=self.model.parameters_dict['backgroundColor'])
+        self.label_diode_unit.pack(side="right")
+
+        self.label_frequency_unit.configure(bg=self.model.parameters_dict['backgroundColor'])
+        self.label_frequency_unit.pack(side="right")
+
+        self.label_period_unit.configure(bg=self.model.parameters_dict['backgroundColor'])
+        self.label_period_unit.pack(side="right")
 
     def initCombo(self):
     #This methods instanciates all the combobox
@@ -362,25 +385,25 @@ class MultimeterView (DeviceFrame):
         self.radio_DCI.pack(side="left", expand="yes", fill="both")
         self.radio_DCI.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.radio_ACI.pack(side="left", expand="yes", fill="both")
+        self.radio_ACI.pack(side="left", expand="yes", fill="y", anchor='w')
         self.radio_ACI.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.radio_2WR.pack(side="left", expand="yes", fill="both")
+        self.radio_2WR.pack(side="left", expand="yes", fill="y", anchor='w')
         self.radio_2WR.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.radio_4WR.pack(side="left", expand="yes", fill="both")
+        self.radio_4WR.pack(side="left", expand="yes", fill="y", anchor='w')
         self.radio_4WR.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.radio_diode.pack(side="left", expand="yes", fill="both")
+        self.radio_diode.pack(side="left", expand="yes", fill="y", anchor='w')
         self.radio_diode.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.radio_continuity.pack(side="left", expand="yes", fill="both")
+        self.radio_continuity.pack(side="left", expand="yes", fill="y", anchor='w')
         self.radio_continuity.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.radio_frequency.pack(side="left", expand="yes", fill="both")
+        self.radio_frequency.pack(side="left", expand="yes", fill="y", anchor='w')
         self.radio_frequency.configure(bg=self.model.parameters_dict['backgroundColor'])
 
-        self.radio_period.pack(side="left", expand="yes", fill="both")
+        self.radio_period.pack(side="left", expand="yes", fill="y", anchor='w')
         self.radio_period.configure(bg=self.model.parameters_dict['backgroundColor'])
 
         self.radio_caliberACmA.pack(side="right", fill="both", anchor='ne')
@@ -394,6 +417,18 @@ class MultimeterView (DeviceFrame):
 
         self.radio_caliberDC10A.pack(side="right", fill="both", anchor='ne')
         self.radio_caliberDC10A.configure(bg=self.model.parameters_dict['backgroundColor'])
+
+        self.radio_caliberDiode5V.pack(side="right", fill="both", anchor='ne')
+        self.radio_caliberDiode5V.configure(bg=self.model.parameters_dict['backgroundColor'])
+
+        self.radio_caliberDiode10V.pack(side="right", fill="both", anchor='ne')
+        self.radio_caliberDiode10V.configure(bg=self.model.parameters_dict['backgroundColor'])
+
+        self.radio_caliberDiode5mA.pack(side="right", fill="both", anchor='ne')
+        self.radio_caliberDiode5mA.configure(bg=self.model.parameters_dict['backgroundColor'])
+
+        self.radio_caliberDiode10mA.pack(side="right", fill="both", anchor='ne')
+        self.radio_caliberDiode10mA.configure(bg=self.model.parameters_dict['backgroundColor'])
 
         self.measure_activate.pack(expand="yes")
 
@@ -437,6 +472,8 @@ class MultimeterView (DeviceFrame):
             child.configure(state="disabled")
         for child in self.frame_diode.winfo_children():
             child.configure(state="disabled")
+        for child in self.frame_diode_caliber.winfo_children():
+            child.configure(state="disabled")
         for child in self.frame_continuity.winfo_children():
             child.configure(state="disabled")
         for child in self.frame_frequency.winfo_children():
@@ -444,16 +481,16 @@ class MultimeterView (DeviceFrame):
         for child in self.frame_period.winfo_children():
             child.configure(state="disabled")
                 
-            self.radio_DCV.configure(fg="grey")
-            self.radio_ACV.configure(fg="grey")
-            self.radio_DCI.configure(fg="grey")
-            self.radio_ACI.configure(fg="grey")
-            self.radio_2WR.configure(fg="grey")
-            self.radio_4WR.configure(fg="grey")
-            self.radio_diode.configure(fg="grey")
-            self.radio_continuity.configure(fg="grey")
-            self.radio_frequency.configure(fg="grey")
-            self.radio_period.configure(fg="grey")
+            self.radio_DCV.configure(fg="grey24")
+            self.radio_ACV.configure(fg="grey24")
+            self.radio_DCI.configure(fg="grey24")
+            self.radio_ACI.configure(fg="grey24")
+            self.radio_2WR.configure(fg="grey24")
+            self.radio_4WR.configure(fg="grey24")
+            self.radio_diode.configure(fg="grey24")
+            self.radio_continuity.configure(fg="grey24")
+            self.radio_frequency.configure(fg="grey24")
+            self.radio_period.configure(fg="grey24")
 
         if self.intVar_radioValueSetup.get() == 0 :            
             for child in self.frame_DCV.winfo_children():
@@ -491,6 +528,8 @@ class MultimeterView (DeviceFrame):
 
         if self.intVar_radioValueSetup.get() == 6:     
             for child in self.frame_diode.winfo_children():
+                child.configure(state="normal")   
+            for child in self.frame_diode_caliber.winfo_children():
                 child.configure(state="normal")
             self.radio_diode.configure(fg="black")
 
@@ -522,7 +561,7 @@ class MultimeterView (DeviceFrame):
 
     def radio_caliber_callback(self):
     #This method is called when the amp caliber is changed 
-        if self.intVar_radioValueCaliber.get() == 0:
+        if self.intVar_radioValueCaliberA.get() == 0:
             self.combo_DCI.configure(value=["1mA", "100uA", "400mA", "100mA", "10mA"])
             self.combo_DCI.current(0)
             self.combo_ACI.configure(value=["10mA", "400mA", "100mA"])
