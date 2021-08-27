d6590332a1b8e6d640f7857ff031068b9e50179a
Luc Perard (LUEA) <luea@oticonmedical.com>
2021-08-27 09:39:37 +0200

    Export CSV added to script

--- MyLab.py
+++ MyLab.py
@@ -1,27 +1,3 @@
-"""Copyright Oticon Medical NICE
-
-Developped by : Luc PERARD
-Version : 0.0
-Details : 
-    - 2021/07/08 Software creation 
-
-File description : Application Launcher
-
-"""
-
-from Controller import Controller
-import sys
-import os
-
-if __name__ == "__main__":
-
-    path = ""
-    company_name = 'Oticon medical'
-    product_version = '0.0'
-    product_name = 'MyLab_V' + product_version
-    
-    if len(sys.argv) > 1:
-        path = sys.argv[1]
-
-    controller = Controller(path)
-    
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3858b346c3eb0ef7bcbf8cfb60a3f268ec95ffac46d7d7b20651008d7411764
+size 376
